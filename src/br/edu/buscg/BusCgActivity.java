74ee28e19fd6d9ffbf39a150a28899adfb70e910
tgmarinho <tgmarinho@gmail.com>
2012-09-05 19:28:29 -0400

    enquanto não tenho um designer segue esse splash que fiz

---
+++ src/br/edu/buscg/BusCgActivity.java
@@ -0,0 +1,15 @@
+package br.edu.buscg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class BusCgActivity extends Activity {
+
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
