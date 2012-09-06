47f783d3aefaaee1a4979a6417f3f5069fc42bfa
tgmarinho <tgmarinho@gmail.com>
2012-09-05 20:25:21 -0400

    bin

--- gen/br/edu/buscg/R.java
+++ gen/br/edu/buscg/R.java
@@ -16,9 +16,17 @@ public final class R {
         public static final int buscg_run=0x7f020002;
         public static final int splashbuscg=0x7f020003;
     }
+    public static final class id {
+        public static final int btn_onde_compro_passe=0x7f050001;
+        public static final int btn_saldo=0x7f050000;
+        public static final int comprar_passe=0x7f050003;
+        public static final int meu_buscg=0x7f050002;
+        public static final int tcc=0x7f050004;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int splashbuscg=0x7f030001;
+        public static final int menu=0x7f030001;
+        public static final int splashbuscg=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
